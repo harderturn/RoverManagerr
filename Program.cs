7b4b185a17e4c2c9d24ffd756c46117f445c350d
Burak Kaçar <mburakkacar@gmail.com>
2021-08-14 16:14:46 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace RoverManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Directions N, S, E, W
+            // Location X, Y
+
+            int maxX = 0;
+            int maxY = 0;
+
+            String firstRoverInformations;
+            String firstRoverActCommands;
+
+            String secondRoverInformations;
+            String secondRoverActCommands;
+
+            Console.WriteLine("Enter the max \"X Y\" values");
+
+            String maxXY = Console.ReadLine();
+
+            maxX = Convert.ToInt32(maxXY.Split(" ")[0]);
+            maxY = Convert.ToInt32(maxXY.Split(" ")[1]);
+
+            //First rover
+            Console.WriteLine("Enter the first rovers first location information:");
+            
+            firstRoverInformations = Console.ReadLine();
+
+            Rover r = new Rover(Convert.ToInt32(firstRoverInformations.Split(" ")[0]), Convert.ToInt32(firstRoverInformations.Split(" ")[1]), firstRoverInformations.Split(" ")[2],maxX,maxY);
+
+            Console.WriteLine("Enter the first rovers act commands:");
+
+            firstRoverActCommands = Console.ReadLine();
+
+
+            //Second rover
+            Console.WriteLine("Enter the second rovers first location information:");
+
+            secondRoverInformations = Console.ReadLine();
+
+            Rover r2 = new Rover(Convert.ToInt32(secondRoverInformations.Split(" ")[0]), Convert.ToInt32(secondRoverInformations.Split(" ")[1]), secondRoverInformations.Split(" ")[2], maxX, maxY);
+
+            Console.WriteLine("Enter the second rovers act commands:");
+
+            secondRoverActCommands = Console.ReadLine();
+
+            //Move rovers
+            r.moveWithCommands(firstRoverActCommands);
+
+            r2.moveWithCommands(secondRoverActCommands);
+
+            Console.WriteLine(r.getRoverPositionInfo());
+
+            Console.WriteLine(r2.getRoverPositionInfo());
+
+            Console.ReadLine();
+
+
+            // Unit test yazimi ile hic ilgilenmedim ancak kodu kendim test ettim :') 
+            
+
+
+
+
+        }
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoverManager
+{
+    class Rover
+    {
+
+        public Rover(int startX, int startY, String startDirection, int xLimit, int yLimit)
+        {
+            _x = startX;
+            _y = startY;
+            _direction = startDirection;
+
+            _xLimit = xLimit;
+            _yLimit = yLimit;
+        }
+
+        // Location
+        private int _x;
+        private int _y;
+        private String _direction;
+
+        // Limits
+        private int _xLimit;
+        private int _yLimit;
+
+        public Rover moveWithCommands(String commandChars) {
+
+            var commandCharArray = commandChars.ToCharArray();
+
+            for (int i = 0; i < commandCharArray.Length; i++)
+            {
+
+                switch (commandCharArray[i])
+                {
+                    case 'L':
+                        switch (this._direction)
+                        {
+                            case "N":
+                                this._direction = "W";
+                                break;
+                            case "S":
+                                this._direction = "E";
+                                break;
+                            case "W":
+                                this._direction = "S";
+                                break;
+                            case "E":
+                                this._direction = "N";
+                                break;
+                            default:
+                                break;
+                        }
+
+
+                        break;
+                    case 'R':
+                        switch (this._direction)
+                        {
+                            case "N":
+                                this._direction = "E";
+                                break;
+                            case "S":
+                                this._direction = "W";
+                                break;
+                            case "W":
+                                this._direction = "N";
+                                break;
+                            case "E":
+                                this._direction = "S";
+                                break;
+                            default:
+                                break;
+                        }
+
+                        break;
+                    case 'M':
+
+                        switch (this._direction)
+                        {
+                            case "N":
+                                this._y +=1;
+                                break;
+                            case "S":
+                                this._y -= 1;
+                                break;
+                            case "W":
+                                this._x -= 1;
+                                break;
+                            case "E":
+                                this._x += 1;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Wrong command, rover set to 0 0 N");
+                        this._x = 0;
+                        this._y = 0;
+                        this._direction = "N";
+                        break;
+
+                    
+                }
+
+                // Limit control.
+                if (this._x > _xLimit || this._y > _yLimit)
+                {
+                    Console.WriteLine("Limits exceeded by rover!");
+                    this._x = 0;
+                    this._y = 0;
+                    this._direction = "N";
+                }
+
+            }
+
+            return this;
+        }
+
+        public String getRoverPositionInfo() {
+            String info = this._x + " " + this._y + " " + this._direction;
+            return info;
+        }
+
+    }
+}
